ce5fe3bf1d3c94458b8b73142810d927169fc72e
S-cloud-cloud <shambhavikashyap8375@gmail.com>
2025-02-06 13:11:20 +0530

    chapter 1 in progress

---
+++ file1.py
@@ -0,0 +1,11 @@
+#1.1
+print('shambhavi kashyap')
+#demo of how python code gets executed:
+print(' \ /')
+print('( O )')
+print('\ncute')
+#line by line execution/interpretion : python is a interpreter
+
+#1.2 
+print('_' * 10)
+
